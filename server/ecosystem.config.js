b93f1b0fc97466607cae95b8231daeb7a5c3d217
sangsang242 <windmill131@naver.com>
2019-10-21 23:24:18 +0900

    Fix minor errors

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'express',
-    script: './bin/www',
+    script: './server/bin/www',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
